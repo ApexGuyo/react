81abfb642f4a204b051688c82d663295589db00a
Guyo-deepsea <samuelguyo21@gmail.com>
2024-04-16 17:03:33 +0300

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 
 import './App.css';
+import Employee from './component/employee';
+import { useState } from 'react';
 
+export default App;
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
+      {showEmployees ? (
+        <>
+        <input
+        type="text" onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }}
+        />
+        <Employee name="guyo" role="intern" />
+        <Employee name="guyo" role={role} />
+        <Employee name="guyo"  />
+        </>
+        
+      ): (
+        <p>you cannot see the employee</p>
+      )
+
+      }
       
-        <p>
-          hello !
-        </p>
-        <a
-          className="App-link"
-          href="https:reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
-
-export default App;
+ 
